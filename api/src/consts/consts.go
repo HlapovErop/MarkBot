4dcedf8bcbaaadb66b9f0ea157e20376591af2a1
epkhlapov <epkhlapov@avito.ru>
2025-04-16 02:49:00 +0300

    fix consts

--- api/src/consts/consts.go
+++ api/src/consts/consts.go
@@ -6,6 +6,6 @@ package consts
 const (
 	DEFAULT_HOST              = "0.0.0.0:3000"
 	TOGGLES_FILE_PATH         = "./src/database/toggles.json"
-	INIT_CAN_REGISTER         = true
+	INIT_CAN_REGISTER         = false
 	POINTS_AFTER_REGISTRATION = 100
 )
